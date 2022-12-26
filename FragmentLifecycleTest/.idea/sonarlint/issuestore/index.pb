5c0b9758b145e6e3453ed1862a425d677f02acc0
parade621 <parade621@naver.com>
2022-12-26 14:38:27 +0900

    Fragment 생명주기 실험을 위한 프로젝트
    프래그먼트를 code에서는 처음으로 FramLayout에 연결해봄..!!

--- FragmentLifecycleTest/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-Y
-)app/src/main/res/layout/activity_main.xml,7\6\76b07e40f24cce093536731477540f12e7da3cb6
-u
-Eapp/src/main/java/com/parade621/fragmentlifecycletest/MainActivity.kt,c\0\c0f0608cab64eb0e397ea36f89fc6cf628dcffda
--- FragmentLifecycleTest/app/src/main/java/com/parade621/fragmentlifecycletest/MainActivity.kt
+++ FragmentLifecycleTest/app/src/main/java/com/parade621/fragmentlifecycletest/MainActivity.kt
@@ -2,10 +2,42 @@ package com.parade621.fragmentlifecycletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("TAG ", "onCreate(), MainActivity")
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.frameContainer, FirstFragment())
+            .commit()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("TAG ", "onStart(), MainActivity")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("TAG ", "onResume(), MainActivity")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("TAG ", "onPause(), MainActivity")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("TAG ", "onStop(), MainActivity")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("TAG ", "onDestroy(), MainActivity")
     }
 }
